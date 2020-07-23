5b8c25f66ae80f0cc522b81de56553cddd46ce97
raymond zhao <raymnzd@gmail.com>
2020-07-23 19:24:09 -0400

    smallest missing positive

--- smallest_missing_positive.py
+++ smallest_missing_positive.py
@@ -5,12 +5,24 @@ def first_missing_positive(nums):
     n = len(nums)
     while i < len(nums):
         j = nums[i] - 1
-        if j <= n and j >= 0 and nums[i] != nums[j]:
+        if j < n and j >= 0 and nums[i] != nums[j]:
             nums[i], nums[j] = nums[j], nums[i]
         else:
             i += 1
 
 
-    print(nums)
+
+    for i in range(n):
+        if nums[i] != i+1:
+            return i+1
+
+    return n + 1 
 
 
+
+def main():
+    print(first_missing_positive([-3, 1, 5, 4, 2]))
+    print(first_missing_positive([-5,-3,-6,-2, 3]))
+
+
+main()
