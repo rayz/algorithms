8a4855e654c8e0cb0375b87e7de0c5fe737b0681
raymond zhao <raymnzd@gmail.com>
2020-07-31 14:20:38 -0400

    pretty cool

---
+++ unionfind.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+class UnionFind:
+
+    def __init__(self):
+        self.parents = {}
+        self.sz = defaultdict(lambda:1) #size of every component
+        self.size = 0
+
+
+    def add(self, a):
+        if a in self.parents:
+            return
+        self.size += 1
+        self.parents[a] = -1
+
+    def find(self, a):
+        if self.parents[a] == -1:
+            return a
+        #path compression
+        self.parents[a] = self.find(self.parents[a])
+        return self.parents[a]
+
+    def join(self, a, b):
+        pa, pb = self.find(a), self.find(b)
+        if pa == pb:
+            return
+        if self.sz[pa] > self.size[pb]:
+            self.parents[pb] = pa
+            self.sz[pb] += self.sz[pa]
+        else:
+            self.parents[pa] = pb 
+            self.sz[pa] += self.sz[pb]
+
+        self.size -= 1
+
+
+
