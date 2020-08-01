e312318a6de513a2c02fbbe0501eaff5e916f6af
raymond zhao <raymnzd@gmail.com>
2020-08-01 16:59:22 -0400

    lcs

--- lcs.py
+++ lcs.py
@@ -15,7 +15,24 @@ for i in range(1, n+1):
         else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
 
-print(dp[-1][-1])
+
+word = [""] * dp[-1][-1]
+
+i = n
+j = m
+index = dp[-1][-1]
+while i > 0 and j > 0:
+    if s[j-1] == t[i-1]:
+        word[index-1] = s[j-1]
+        j -= 1
+        i -= 1
+        index -= 1
+    elif dp[i-1][j] > dp[i][j-1]:
+        i -= 1 
+    else:
+        j -= 1
+
+print("".join(word))
 
 
 
