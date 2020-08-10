0315f6b79ab22e82b24b13cf840c3a4e1ba35001
raymond zhao <raymnzd@gmail.com>
2020-08-09 23:11:14 -0400

    prefix+suffix

--- prefix_suffix.py
+++ prefix_suffix.py
@@ -5,11 +5,17 @@ def prefix_sum(nums):
         prefix.append(prefix[-1] + n)
     return prefix
 
-
+def suffix_sum(nums):
+    n = len(nums)
+    suffix = [0] * (n+1) 
+    for i in range(n-1, -1, -1):
+        suffix[i] = suffix[i+1] + nums[i]
+    return suffix
 
 def main():
     nums = [1,2,3,4,5]
     print(prefix_sum(nums))
+    print(suffix_sum(nums))
 
 
 main()
