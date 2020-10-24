9f1e3c46dea39f8f76039338ada7150b17103532
raymond zhao <raymnzd@gmail.com>
2020-10-23 22:35:56 -0400

    update

--- template.cpp
+++ template.cpp
@@ -1,14 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cctype>
-#include <stack>
-#include <string>
-#include <map>
-#include <queue>
-#include <set>
-#include <math.h>
-#include <iomanip>
+#include <bits/stdc++.h>
 
 using namespace std;
 
