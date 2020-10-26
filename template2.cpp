4ea0812c0a14ff0eca76e52aa603fa13938dc76f
raymond zhao <raymnzd@gmail.com>
2020-10-26 00:08:18 -0400

    update

--- template2.cpp
+++ template2.cpp
@@ -1,5 +1,9 @@
+/*
+ID: raymnzd1 
+TASK: test
+LANG: C++
+*/
 #include <bits/stdc++.h>
-
 using namespace std;
 
 typedef long long ll;
@@ -8,10 +12,9 @@ const long long MOD = 1e9 + 7;
 const int INF = 1e9;
 const ll LINF = 1e18;
 
-
-
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
 }
